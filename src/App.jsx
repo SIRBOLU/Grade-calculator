13134893a5b6b01eb19f5955683077ab89b9023b
Sirbolu <peterbolu@yahoo.com>
2026-07-02 14:06:42 +0100

    transition

--- src/App.jsx
+++ src/App.jsx
@@ -233,6 +233,24 @@ const App = () => {
     return "F";
   };
 
+  //   const scoreRange = (score) => {
+  //     if (score >= 80) {
+  //       return "A+";
+  //     } else if (score >= 70) {
+  //       return "A";
+  //     } else if (score >= 60) {
+  //       return "B";
+  //     } else if (score >= 50) {
+  //       return "C";
+  //     } else if (score >= 45) {
+  //       return "D";
+  //     } else if (score >= 40) {
+  //       return "E";
+  //     } else {
+  //       return "F";
+  //     }
+  //   };
+
   const handleSubjectChange = (index, value) => {
     const newSubjects = [...subjects];
     newSubjects[index].subject = value;
@@ -319,137 +337,6 @@ const App = () => {
 
   const lowestScore = validScores.length > 0 ? Math.min(...validScores) : 0;
 
-  // return (
-  //   <div className="bg-green-200 h-screen flex justify-center items-center">
-  //     <div className="bg-green-300 p-10 rounded-2xl border-2 border-green-700 w-[850px]">
-  //       <h1 className="text-3xl text-green-800 font-extrabold text-center pb-6">
-  //         Grade Calculator
-  //       </h1>
-
-  //       <p className="italic">
-  //         This helps students easily calculate their overall grade (%)
-  //       </p>
-
-  //       <p className="mb-6">
-  //         Input your course/subject and the corresponding score.
-  //       </p>
-
-  //       {/* Headings */}
-  //       <div className="flex justify-between px-4 font-bold text-green-900 mb-2">
-  //         <div className="w-[30px] text-center">No.</div>
-
-  //         <div className="w-[180px]">Subject</div>
-
-  //         <div className="w-[70px] text-center">Score</div>
-
-  //         <div className="w-[60px] text-center">Grade</div>
-
-  //         <div className="w-[90px] text-center">Actions</div>
-  //       </div>
-
-  //       {/* Rows */}
-  //       <div className="max-h-[40vh] overflow-y-auto pr-2">
-  //         {subjects.map((item, index) => (
-  //           <div
-  //             key={index}
-  //             className="flex justify-between items-start px-4 py-2"
-  //           >
-  //             {/* Number */}
-  //             <div className="w-[30px] text-center font-bold text-green-800 pt-2">
-  //               {index + 1}
-  //             </div>
-
-  //             {/* Subject */}
-  //             <div>
-  //               <input
-  //                 className="w-[180px] p-1 border-2 border-green-700"
-  //                 type="text"
-  //                 value={item.subject}
-  //                 onChange={(e) => handleSubjectChange(index, e.target.value)}
-  //               />
-  //             </div>
-
-  //             {/* Score */}
-  //             <div>
-  //               <input
-  //                 className={`w-[70px] p-1 border-2 text-center ${
-  //                   item.error ? "border-red-500" : "border-green-700"
-  //                 }`}
-  //                 type="number"
-  //                 min="0"
-  //                 max="100"
-  //                 value={item.score}
-  //                 onChange={(e) => handleScoreChange(index, e.target.value)}
-  //               />
-
-  //               {item.error && (
-  //                 <p className="text-red-600 text-xs mt-1 w-[120px]">
-  //                   {item.error}
-  //                 </p>
-  //               )}
-  //             </div>
-
-  //             {/* Grade */}
-  //             <div>
-  //               <p className="w-[60px] h-[36px] flex items-center justify-center border-2 border-green-700 bg-white">
-  //                 {item.grade}
-  //               </p>
-  //             </div>
-
-  //             {/* Buttons */}
-  //             <div className="flex gap-2">
-  //               <button
-  //                 className="bg-green-600 text-white w-8 h-8 rounded-full hover:bg-green-700"
-  //                 onClick={addRow}
-  //               >
-  //                 +
-  //               </button>
-
-  //               <button
-  //                 className="bg-red-600 text-white w-8 h-8 rounded-full hover:bg-red-700"
-  //                 onClick={() => deleteRow(index)}
-  //               >
-  //                 -
-  //               </button>
-  //             </div>
-  //           </div>
-  //         ))}
-  //       </div>
-
-  //       {/* Statistics */}
-  //       <div className="mt-6 border-t-2 border-green-700 pt-4">
-  //         <h2 className="text-xl font-bold mb-3">Statistics</h2>
-
-  //         <p>
-  //           <strong>Total Subjects:</strong> {totalSubjects}
-  //         </p>
-
-  //         <p>
-  //           <strong>Average Score:</strong> {averageScore}%
-  //         </p>
-
-  //         <p>
-  //           <strong>Highest Score:</strong> {highestScore}
-  //         </p>
-
-  //         <p>
-  //           <strong>Lowest Score:</strong> {lowestScore}
-  //         </p>
-  //       </div>
-
-  //       {/* Reset Button */}
-  //       <div className="flex justify-end mt-6">
-  //         <button
-  //           className="bg-red-600 text-white px-4 py-2 rounded-md text-sm hover:bg-red-700"
-  //           onClick={resetForm}
-  //         >
-  //           Reset
-  //         </button>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
-
   return (
     // <div className="min-h-screen bg-slate-100 flex items-center justify-center p-6">
     //   <div className="w-full max-w-5xl bg-white rounded-2xl shadow-2xl overflow-hidden">
@@ -506,6 +393,7 @@ const App = () => {
                   <input
                     type="text"
                     value={item.subject}
+                    maxLength={40}
                     onChange={(e) => handleSubjectChange(index, e.target.value)}
                     placeholder="Enter subject"
                     className="border rounded-lg px-3 py-2 focus:ring-2 focus:ring-green-500 focus:outline-none"
@@ -595,7 +483,7 @@ const App = () => {
 
             <button
               onClick={resetForm}
-              className="bg-red-500 hover:bg-red-600 text-white px-5 py-2 rounded-lg shadow"
+              className="bg-red-600 hover:bg-red-500 text-white px-5 py-2 rounded-lg shadow"
             >
               Reset
             </button>
@@ -604,7 +492,7 @@ const App = () => {
           {/* Statistics */}
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-8">
             {/* Total Subjects */}
-            <div className="bg-green-50 border border-green-200 rounded-xl p-4 shadow-sm hover:shadow-md transition">
+            <div className="bg-green-50 border border-green-200 rounded-xl p-4 shadow-sm hover:shadow-xl transition-transform duration-300 hover:scale-[1.1]">
               <p className="text-gray-500 text-sm">📚 Total Subjects</p>
               <h2 className="text-3xl font-bold text-green-700 mt-2">
                 {totalSubjects}
@@ -612,7 +500,7 @@ const App = () => {
             </div>
 
             {/* Average */}
-            <div className="bg-blue-50 border border-blue-200 rounded-xl p-4 shadow-sm hover:shadow-md transition">
+            <div className="bg-blue-50 border border-blue-200 rounded-xl p-4 shadow-sm hover:shadow-md transition-transform duration-300 hover:scale-[1.1]">
               <p className="text-gray-500 text-sm">📈 Average Score</p>
               <h2 className="text-3xl font-bold text-blue-700 mt-2">
                 {averageScore}%
@@ -620,7 +508,7 @@ const App = () => {
             </div>
 
             {/* Highest */}
-            <div className="bg-yellow-50 border border-yellow-200 rounded-xl p-4 shadow-sm hover:shadow-md transition">
+            <div className="bg-yellow-50 border border-yellow-200 rounded-xl p-4 shadow-sm hover:shadow-md transition-transform duration-300 hover:scale-[1.1]">
               <p className="text-gray-500 text-sm">🏆 Highest Score</p>
               <h2 className="text-3xl font-bold text-yellow-600 mt-2">
                 {highestScore}
@@ -628,7 +516,7 @@ const App = () => {
             </div>
 
             {/* Lowest */}
-            <div className="bg-red-50 border border-red-200 rounded-xl p-4 shadow-sm hover:shadow-md transition">
+            <div className="bg-red-50 border border-red-200 rounded-xl p-4 shadow-sm hover:shadow-md transition-transform duration-300 hover:scale-[1.1]">
               <p className="text-gray-500 text-sm">📉 Lowest Score</p>
               <h2 className="text-3xl font-bold text-red-600 mt-2">
                 {lowestScore}
